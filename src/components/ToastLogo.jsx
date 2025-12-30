27d11b10efb9bd20f8da867f10bda68617ff7baa
priyanshnihalani <priyansh.nihalani@gmail.com>
2025-12-30 18:59:00 +0530

    [PN][Toast Added]

---
+++ src/components/ToastLogo.jsx
@@ -0,0 +1,14 @@
+const ToastLogo = () => (
+    <img
+        src={"/logo.png"}
+        alt="logo"
+        style={{
+            width: 24,
+            height: 24,
+            borderRadius: "4px",
+            objectFit: "contain",
+        }}
+    />
+);
+
+export default ToastLogo
